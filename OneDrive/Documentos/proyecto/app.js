ab2858b777f404459042a4b8a00d236c415c71e9
gallardo034 <gallardoagustinm@gmail.com>
2026-05-12 22:36:22 -0300

    nuevo diseño portfolio

---
+++ OneDrive/Documentos/proyecto/app.js
@@ -0,0 +1,6 @@
+let arrayDePeliculas = require('./peliculas');
+//console.log(arrayDePeliculas);
+
+for(var i = 0; i < arrayDePeliculas.length; i++){
+    console.log(arrayDePeliculas[i].genre);
+}
---
+++ OneDrive/Documentos/proyecto/peliculas.js
@@ -0,0 +1,19 @@
+// micro desafio 1, COMPRA Y VENTA DE PELICULAS
+
+let peliculas = [
+    {id : 'Joker', rating : 8.4, awards : '2 premios Oscar', length : '122 minutos' , price : 'USD 55,000,000' ,genre : 'suspenso' },
+    
+    {id : 'Toy Story 3 ', rating : 8.3, awards : '2 premios Oscar', length : '103 minutos' , price : 'USD 200,000,000' ,genre : 'animacion' },
+    
+    {id : 'Top Gun', rating : 8.3, awards : '1 premio Oscar', length : '130 minutos' , price : 'USD 170,000,000' ,genre : 'accion' },
+    
+    {id : 'Son Como Niños', rating : 5.9, awards : '3 premios', length : '102 minutos' , price : 'USD 80,000,000' ,genre : 'comedia' },
+    
+    {id : 'Up', rating : 8.3, awards : '2 premios Oscar', length : '96 minutos' , price : 'USD 175,000,000' ,genre : 'aventura' },
+]
+
+module.exports = peliculas;
+
+
+
+
